82a8b06dbdbb89be354ea31975595f86e0a67161
alisa69 <aliska132007@gmail.ru>
2025-09-19 21:28:47 +0500

    Task1

---
+++ Tyuiu.RadchenkoAV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Task;
+namespace Tyuiu.RadchenkoAV.Sprint0.Task1.V0
+{ 
+    0 references
+    class Program
+    {
+        0 references
+        static void Main(string[] args)
+        {
+            Console.Wrireline("Hello, World");
+
+            string name = "Алиса";
+            int age = 18;
+
+            Console.Writelen("Здравствуйте меня зовут" + name + "мне" + age + "лет.");
+
+            Console.ReadKey();
+        }
+    }
+}
+   
