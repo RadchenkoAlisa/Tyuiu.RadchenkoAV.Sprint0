1cb56da89b440c25d958a659ae7dce432d5e6231
alisa69 <aliska132007@gmail.ru>
2025-10-01 02:16:27 +0500

    Task1

---
+++ Tyuiu.RadchenkoAV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace Tyuiu.RadchenkoAV.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Алиса";
+            int age = 45;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
